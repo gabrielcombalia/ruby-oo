419d4b5a76a2376301bae1317e9aea39c2055f06
gabrielcombali1 <gabrielcombalia@users.noreply.github.com>
2022-10-22 18:17:16 +0000

    modo correto em ruby

--- conta.rb
+++ conta.rb
@@ -1,22 +1,12 @@
 class Conta
 
+  attr_reader :numero, :titular
+  attr_accessor :saldo
   
   def initialize(numero, titular, saldo)
     @numero = numero
     @titular = titular
     @saldo = saldo
-  end
-
-  def numero_titular
-    @numero
-  end
-
-  def nome_titular
-    @titular
-  end
-
-  def altera_nome=(novo_nome)
-    @titular = novo_nome
   end
   
 end
